efc7391c4e198518cd6eb5fa4b0797a0c6f4f633
Nhan-07 <phantannhan0701@gmail.com>
2025-03-12 10:42:06 +0700

    đã xong

---
+++ S4 CNPM/cong viec 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S4_CNPM
+{
+    public partial class cong_viec_2 : Form
+    {
+        public cong_viec_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
